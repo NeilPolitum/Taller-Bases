f2eed90b5c2337c826e987c2e3324f7faf29d2e4
Daniel Rubiano <dsrubianop@correo.udistrital.edu.co>
2022-11-09 00:32:16 -0500

    equisde

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import axios from 'axios';
 
@@ -14,25 +14,18 @@ function Info() {
       setDatos(response.data);
     });
     console.log(datos)
-  }, []);
+  }, [correo,datos]);
   
 
   return (
-    <form onSubmit={enviarDatos}>
+    <form>
       <div className='login'>
         <h1>Registrar</h1>
         <input disabled className='boton' type="text" placeholder={datos.nombre} name="nombre" />
-        <input className='boton' type="text" placeholder='Apellido' onChange={handleDatos} name="apellido" />
-        <select className='boton' onChange={handleDatos} name='tipo_documento'>
-          <option disabled selected>Tipo Documento</option>
-          <option value="CC">C.C.</option>
-          <option value="NIT">NIT</option>
-          <option value="CE">C.E.</option>
-        </select>
-        <input className='boton' type="text" placeholder="Numero Documento" onChange={handleDatos} name="numero_documento" />
-        <input className='boton' type="text" placeholder='Correo' onChange={handleDatos} name="correo" />
-        <input className='boton' type="password" placeholder='Contraseña' onChange={handleDatos} name="clave" />
-        <button type="submit" className='boton'>Enviar</button>
+        <input disabled className='boton' type="text" placeholder={datos.apellido} name="apellido" />
+        <input disabled className='boton' type="text" placeholder={datos.tipo_documento} name="tipo_documento" />
+        <input disabled className='boton' type="text" placeholder={datos.numero_documento} name="numero_documento" />
+        <input disabled className='boton' type="text" placeholder={datos.correo} name="correo" />
       </div>
     </form>
   );
